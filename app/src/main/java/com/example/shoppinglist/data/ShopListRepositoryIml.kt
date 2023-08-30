bae096bc5363d34bc66b95bcc1f548effb6e1825
Lavrentyev Pavel <pa.lavrentyev@gmail.com>
2023-08-30 15:41:44 +0300

    Data commit

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryIml.kt
@@ -0,0 +1,42 @@
+package com.example.shoppinglist.data
+
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryIml: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+    private var autoIncrementId = 0
+    override fun addShopItem(shopItem: ShopItem) {
+        //TODO("Not yet implemented")
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopItem.id = autoIncrementId++
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        //TODO("Not yet implemented")
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        //TODO("Not yet implemented")
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        //TODO("Not yet implemented")
+        return shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id.$shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        //TODO("Not yet implemented")
+        return shopList.toList()
+    }
+}
