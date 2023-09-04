be10e00b0795047960a49bbcdbce93fc58753e70
Lavrentyev Pavel <pa.lavrentyev@gmail.com>
2023-09-04 13:22:00 +0300

    ViewType & RecyclerViewPool.

--- app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.shoppinglist.presentation
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import com.example.shoppinglist.domain.ShopItem
 
 class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
+    var count = 0
     var shopList = listOf<ShopItem>()
         set(value) {
             field = value
@@ -18,11 +20,13 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_shop_disabled,
-            parent,
-            false
-        )
+        Log.d("ShopListAdapter","onCreateViewHolder, count: ${++count}")
+        val layout = when (viewType) {
+            VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
+            VIEW_TYPE_ENABLED -> R.layout.item_shop_enabled
+            else -> throw RuntimeException("Unknown view type: $viewType")
+        }
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
         return ShopItemViewHolder(view)
     }
 
@@ -30,9 +34,14 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
         return shopList.size
     }
 
-    /*override fun getItemViewType(position: Int): Int {
-        return position
-    }*/
+    override fun getItemViewType(position: Int): Int {
+        val  item = shopList[position]
+        return if (item.enabled) {
+            VIEW_TYPE_ENABLED
+        } else {
+            VIEW_TYPE_DISABLED
+        }
+    }
 
     override fun onViewRecycled(viewHolder: ShopItemViewHolder) {
         super.onViewRecycled(viewHolder)
@@ -43,26 +52,11 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
 
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
         val shopItem = shopList[position]
-        val status = if (shopItem.enabled) {
-            "Activ"
-        } else {
-            "Not activ"
-        }
-        //viewHolder.tvName.text = "${shopItem.name} $status"
-        //viewHolder.tvCount.text = shopItem.count.toString()
         viewHolder.view.setOnClickListener {
             true
         }
-
-        if (shopItem.enabled) {
-            viewHolder.tvName.text = "${shopItem.name} $status"
-            viewHolder.tvCount.text = shopItem.count.toString()
-            viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context, android.R.color.holo_red_light))
-        } /*else {
-            viewHolder.tvName.text = ""
-            viewHolder.tvCount.text = ""
-            viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context, android.R.color.white))
-        }*/
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
     }
 
     class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
@@ -70,4 +64,27 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
         val tvCount = view.findViewById<TextView>(R.id.tv_count)
     }
 
+    companion object {
+
+        const val VIEW_TYPE_ENABLED = 100
+        const val VIEW_TYPE_DISABLED = 101
+        const val MAX_POOL_SIZE = 20
+    }
+
 }
+
+// Метод getItemViewType - переопределяет его,
+// если для разных элементов используется рахные макеты.
+
+
+// Значение ViewType используйте внутри метода onCreateViewHolder.
+
+// ViewHolder скрытый с экрана отправляется в пул вьюхолдеров.
+
+// RecyclerView не может переиспользовать ViewHolder,
+// если его тип отличается от типа элемента, который нужно отобразить.
+
+// Пул вьюхолдеров имеет ограниченный размерю Если он заполнен, то при добавлении
+// новых вьюхолдеров чтарые будут удаляться.
+
+// Размер пула вьюхолдера можно увеличить для каждого ViewType.
